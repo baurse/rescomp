eac363cf5ba153fffe5c25a1dc21497eecb61d1e
Duncan, Dennis <DuncDennis@gmail.com>
2022-01-17 11:12:56 +0100

    New KS Simulations Test

--- rescomp/simulations.py
+++ rescomp/simulations.py
@@ -565,7 +565,7 @@ def _kuramoto_sivashinsky_old(dimensions, system_size, dt, time_steps):
     return uu
 
 
-def _kuramoto_sivashinsky(dimensions, system_size, dt, time_steps, starting_point, eps=0, **kwargs):
+def _kuramoto_sivashinsky(dimensions, system_size, dt, time_steps, starting_point, eps=0, M=64, **kwargs):
     """ This function simulates the Kuramoto–Sivashinsky PDE
 
     Even though it doesn't use the RK4 algorithm, it is bundled with the other
@@ -613,7 +613,7 @@ def _kuramoto_sivashinsky(dimensions, system_size, dt, time_steps, starting_poin
     L = (1 + eps) * k ** 2 - k ** 4
     E = np.exp(h * L)
     E_2 = np.exp(h * L / 2)
-    M = 64  # changed because the previous optimizations were negligable and could result in errors for small size systems
+    # M = 64  # changed because the previous optimizations were negligable and could result in errors for small size systems
     r = np.exp(1j * np.pi * (np.arange(1, M + 1) - 0.5) / M)
     LR = h * np.transpose(np.repeat([L], M, axis=0)) + np.repeat([r], n, axis=0)
     Q = h * np.real(np.mean((np.exp(LR / 2) - 1) / LR, axis=1))
@@ -653,7 +653,7 @@ def _kuramoto_sivashinsky(dimensions, system_size, dt, time_steps, starting_poin
 
 
 def _kuramoto_sivashinsky_custom(dimensions, system_size, dt, time_steps, starting_point,
-                                 precision=None, fft_type=None, **kwargs):
+                                 precision=None, fft_type=None, M=64, **kwargs):
     """ This function simulates the Kuramoto–Sivashinsky PDE with custom precision and fft backend"""
 
     if precision is None:
@@ -738,7 +738,7 @@ def _kuramoto_sivashinsky_custom(dimensions, system_size, dt, time_steps, starti
     if change_precision: E = E.astype(f_dtype)
     E_2 = np.exp(h * L / 2)
     if change_precision: E_2 = E_2.astype(f_dtype)
-    M = 64
+    # M = 64
     r = np.exp(1j * np.pi * (np.arange(1, M + 1) - 0.5) / M)
     if change_precision: r = r.astype(c_dtype)
     LR = h * np.transpose(np.repeat([L], M, axis=0)) + np.repeat([r], n, axis=0)
@@ -818,7 +818,7 @@ def _kuramoto_sivashinsky_Bhatt(dimensions, system_size, dt, time_steps, startin
 
     # Define the variable names as in the paper
     N = dimensions
-    M = time_steps - 1
+    M = time_steps
     h = system_size/(dimensions-1)
     k = dt
 
@@ -875,7 +875,7 @@ def _kuramoto_sivashinsky_Bhatt(dimensions, system_size, dt, time_steps, startin
     bracket = a * L2.dot(M2) + b * np.linalg.matrix_power(M2, 2)
     L = L_2_inv_sq.dot(bracket)
 
-    sim_data = np.zeros((time_steps, dimensions))
+    sim_data = np.zeros((M + 1, dimensions))
     sim_data[0, :] = starting_point
     for i in range(1, M + 1):
         u_prev = sim_data[i - 1, :]
@@ -911,3 +911,88 @@ def _kuramoto_sivashinsky_Bhatt(dimensions, system_size, dt, time_steps, startin
         sim_data[i, :] = u_next
 
     return sim_data
+
+def _kuramoto_sivashinsky_alt(dimensions, system_size, dt, time_steps, starting_point, M=16, **kwargs):
+    '''
+    Github: Python implementation at: https://github.com/E-Renshaw/kuramoto-sivashinsky
+    Args:
+        dimensions:
+        system_size:
+        dt:
+        time_steps:
+        starting_point:
+        **kwargs:
+
+    Returns:
+    '''
+
+    # KSequ.m - solution of Kuramoto-Sivashinsky equation
+    #
+    # u_t = -u*u_x - u_xx - u_xxxx, periodic boundary conditions on [0,32*pi]
+    # computation is based on v = fft(u), so linear term is diagonal
+    #
+    # Using this program:
+    # u is the initial condition
+    # h is the time step
+    # N is the number of points calculated along x
+    # a is the max value in the initial condition
+    # b is the min value in the initial condition
+    # x is used when using a periodic boundary condition, to set up in terms of
+    #   pi
+
+    # Initial condition and grid setup
+    N = dimensions# 1024
+
+    if starting_point is None:
+        x = np.transpose(np.conj(np.arange(1, N + 1))) / N
+        u = np.cos(x / system_size) * (1 + np.sin(x / system_size))
+    else:
+        u = starting_point
+
+    # x = np.transpose(np.conj(np.arange(1, N + 1))) / N
+    a = -1
+    b = 1
+    # u = np.cos(x / 16) * (1 + np.sin(x / 16))
+    # u = np.cos(x / system_size) * (1 + np.sin(x / system_size))
+    v = np.fft.fft(u)
+    # scalars for ETDRK4
+    h = dt # 0.25
+    # k = np.transpose(np.conj(np.concatenate((np.arange(0, N / 2), np.array([0]), np.arange(-N / 2 + 1, 0))))) / 16
+    k = np.transpose(np.conj(np.concatenate((np.arange(0, N / 2), np.array([0]), np.arange(-N / 2 + 1, 0))))) / system_size
+    L = k ** 2 - k ** 4
+    E = np.exp(h * L)
+    E_2 = np.exp(h * L / 2)
+    # M = 16
+    r = np.exp(1j * np.pi * (np.arange(1, M + 1) - 0.5) / M)
+    LR = h * np.transpose(np.repeat([L], M, axis=0)) + np.repeat([r], N, axis=0)
+    Q = h * np.real(np.mean((np.exp(LR / 2) - 1) / LR, axis=1))
+    f1 = h * np.real(np.mean((-4 - LR + np.exp(LR) * (4 - 3 * LR + LR ** 2)) / LR ** 3, axis=1))
+    f2 = h * np.real(np.mean((2 + LR + np.exp(LR) * (-2 + LR)) / LR ** 3, axis=1))
+    f3 = h * np.real(np.mean((-4 - 3 * LR - LR ** 2 + np.exp(LR) * (4 - LR)) / LR ** 3, axis=1))
+    # main loop
+    uu = np.array([u])
+    tt = 0
+    # tmax = 150
+    tmax = time_steps*h
+    print(tmax)
+    nmax = round(tmax / h)
+    # nplt = int((tmax / 100) / h)
+    # nplt = int((tmax / dev_fact) / h)
+    g = -0.5j * k
+    for n in range(1, nmax + 1):
+        t = n * h
+        Nv = g * np.fft.fft(np.real(np.fft.ifft(v)) ** 2)
+        a = E_2 * v + Q * Nv
+        Na = g * np.fft.fft(np.real(np.fft.ifft(a)) ** 2)
+        b = E_2 * v + Q * Na
+        Nb = g * np.fft.fft(np.real(np.fft.ifft(b)) ** 2)
+        c = E_2 * a + Q * (2 * Nb - Nv)
+        Nc = g * np.fft.fft(np.real(np.fft.ifft(c)) ** 2)
+        v = E * v + Nv * f1 + 2 * (Na + Nb) * f2 + Nc * f3
+        # if n % nplt == 0:
+        u = np.real(np.fft.ifft(v))
+        uu = np.append(uu, np.array([u]), axis=0)
+        tt = np.hstack((tt, t))
+    uu = np.array(uu)
+
+    return uu
